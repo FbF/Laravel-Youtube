d5a1b962917fadddef3d108cffa4a268a90a8543
Muhamed Didovic <muhamed.didovic@gmail.com>
2017-03-07 08:26:04 +0100

    test

--- src/Fbf/LaravelYoutube/LaravelYoutubeServiceProvider.php
+++ src/Fbf/LaravelYoutube/LaravelYoutubeServiceProvider.php
@@ -2,48 +2,53 @@
 
 use Illuminate\Support\ServiceProvider;
 
-class LaravelYoutubeServiceProvider extends ServiceProvider {
+class LaravelYoutubeServiceProvider extends ServiceProvider
+{
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->package('fbf/laravel-youtube');
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //$this->package('fbf/laravel-youtube');
+        $this->publishes(
+            [
+                __DIR__ . '/config' => config_path('laravel-youtube'),
+            ]
+        );
 
-		include __DIR__.'/../../routes.php';
-	}
+        include __DIR__ . '/../../routes.php';
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->app['youtube'] = $this->app->share(function()
-		{
-			return new Youtube(new \Google_Client);
-		});
-	}
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app['youtube'] = $this->app->share(function () {
+            return new Youtube(new \Google_Client);
+        });
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
-	}
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array();
+    }
 
 }
