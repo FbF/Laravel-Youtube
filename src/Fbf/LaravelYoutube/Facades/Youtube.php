466d0cd618e3aba8a78b52706fc51f232aef3bb1
Muhamed Didovic <muhamed.didovic@gmail.com>
2017-03-15 14:13:13 +0100

    updates

--- src/Fbf/LaravelYoutube/Facades/Youtube.php
+++ src/Fbf/LaravelYoutube/Facades/Youtube.php
@@ -1,11 +1,17 @@
 <?php namespace Fbf\LaravelYoutube\Facades;
 
-class Youtube extends \Illuminate\Support\Facades\Facade {
-	/**
-	 * Get the registered name of the component.
-	 *
-	 * @return string
-	 */
-	protected static function getFacadeAccessor() { return 'youtube'; }
+use Fbf\LaravelYoutube\Contracts\Youtube as YoutubeContract;
+use Illuminate\Support\Facades\Facade;
 
+class Youtube extends Facade {
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    //protected static function getFacadeAccessor() { return 'youtube'; }
+    protected static function getFacadeAccessor()
+    {
+        return YoutubeContract::class;
+    }
 }
